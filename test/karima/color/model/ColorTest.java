e46bd858c2f0e16e56f84f2f7b6a21b7d9177159
imokarima <karima.imoussoura@gmail.com>
2022-03-01 17:02:09 +0100

    9 tests réussis

--- test/karima/color/model/ColorTest.java
+++ test/karima/color/model/ColorTest.java
@@ -8,69 +8,70 @@ class ColorTest {
     private Color color;
 
 
+    @BeforeEach
+    public void setUp() {
+        color = new Color(53, 213, 141);
+    }
+
+    @AfterEach
+    public void tearDown() {
+        color = null;
+    }
     @Test
     void testGetRouge() {
-        assertEquals(213,color.getRouge());
+        assertEquals(53,color.getRouge());
     }
     @Test
     void testGetVert() {
-        assertEquals(141,color.getVert());
+        assertEquals(213,color.getVert());
     }
     @Test
     void testGetBleue() {
-        assertEquals(53,color.getBleu());
+        assertEquals(141,color.getBleu());
     }
     @Test
     void testGetHexValue() {
-        assertEquals("#D58D35",color.getHexValue());
+        assertEquals("#35D58D",color.getHexValue());
     }
-
-
 
     @Test
     public void testSetRouge() {
-        color.setRouge(213);
-        assertEquals( 25, color.getRouge(),"Incorrect Value");
+        color.setRouge(53);
+        assertAll("Color object non conformes",
+                () -> assertEquals(53, color.getRouge(), "getRed() est incorrect"),
+                () -> assertEquals(213, color.getVert(), "getGreen() est incorrect"),
+                () -> assertEquals(141, color.getBleu(), "getBlue() est incorrect"),
+                () -> assertEquals("#35D58D", color.getHexValue(), "getHexValue() est incorrect"));
     }
 
     @Test
     public void testSetBleue() {
         color.setBleu(141);
-        assertEquals( 25, color.getBleu(),"Incorrect Value");
+        assertAll("Color object non conformes",
+                ()-> assertEquals(53, color.getRouge(),"getRed() est incorrect"),
+                ()-> assertEquals(213, color.getVert(),"getGreen() est incorrect"),
+                ()-> assertEquals(141, color.getBleu(), "getBlue() est incorrect"),
+                ()-> assertEquals("#35D58D", color.getHexValue(), "getHexValue() est incorrect"));
     }
     @Test
     public void testSetVert() {
-        color.setVert(53);
-        assertEquals( 25, color.getVert(),"Incorrect Value");
+        color.setVert(213);
+        assertAll("Color object non conformes",
+                ()-> assertEquals(53, color.getRouge(),"getRed() est incorrect"),
+                ()-> assertEquals(213, color.getVert(),"getGreen() est incorrect"),
+                ()-> assertEquals(141, color.getBleu(), "getBlue() est incorrect"),
+                ()-> assertEquals("#35D58D", color.getHexValue(), "getHexValue() est incorrect"));
     }
     @Test
     public void testSetHexValue() {
-        color.setHexValue("#D58D35");
-        assertEquals( "#D58D90", color.getHexValue(),"Incorrect Value");
+        color.setHexValue("#35D58D");
+        assertEquals( "#35D58D", color.getHexValue(),"Incorrect Value");
     }
     @Test
     public void testToString() {
-        assertEquals("value:#D58D90, rouge:53, vert:213,bleu:141", color.toString());
+        assertEquals("[value:#35D58D, rouge:53, vert:213, bleu:141]", color.toString());
     }
 
 
-    @BeforeAll
-    static void beforeAll() {
 
-    }
-
-    @AfterAll
-    static void afterAll() {
-
-    }
-
-    @BeforeEach
-    void setUp() {
-        color=new Color(213, 141, 53);
-    }
-
-    @AfterEach
-    void tearDown() {
-        color=null;
-    }
 }
