19af98a5cd338e330aaa1bae5102a8ab144a0c3b
imokarima <karima.imoussoura@gmail.com>
2022-03-02 11:22:37 +0100

    36 tests réussis et version plus amélioré de la class Color

--- src/karima/color/model/Color.java
+++ src/karima/color/model/Color.java
@@ -1,31 +1,28 @@
 package karima.color.model;
 
+import java.util.regex.Pattern;
+
 public class Color {
     private int rouge;
     private int vert;
     private int bleu;
     private String hexValue;
+    private static final short MIN = 0;
+    private static final short MAX= 255;
+
 
     public Color(int rouge, int vert, int bleu){
-        if((rouge<0 && rouge>255) &&(vert<0 && vert>255)&&(bleu<0 && bleu>255)){
-            throw new IllegalArgumentException();
-        }else{
-            this.rouge = rouge;
-            this.vert = vert;
-            this.bleu = bleu;
-            this.hexValue=String.format("#%02X%02X%02X", this.rouge, this.vert,this.bleu);
-        }
+        setRouge(rouge);
+        setVert(vert);
+        setBleu(bleu);
+        this.hexValue=String.format("#%02X%02X%02X", this.rouge, this.vert,this.bleu);
     }
 
     public Color(String hexValue){
-        if(hexValue.length()>0 && hexValue.length()<7 && hexValue.startsWith("#")){
-            this.hexValue=hexValue;
+            setHexValue(hexValue);
             this.rouge=Integer.parseInt(hexValue.substring(1,3),16);
             this.vert=Integer.parseInt(hexValue.substring(3,5),16);
             this.bleu=Integer.parseInt(hexValue.substring(5,7),16);
-        }else if(hexValue==null) {
-            throw new IllegalArgumentException();
-        }
     }
 
     public int getRouge() {
@@ -44,25 +41,49 @@ public class Color {
         return hexValue;
     }
 
+
+    private boolean isCorrectHexValue(String hexValue) {
+        return Pattern.matches("^#([0-9A-F]{6})$", hexValue);
+    }
+
+    private boolean isCorrectRGBValue(int RGBvalue) {
+        return MIN<= RGBvalue && RGBvalue <= MAX;
+    }
+
     public void setHexValue(String hexValue) {
-        this.hexValue = hexValue;
+        if (hexValue == null || !isCorrectHexValue(hexValue)) {
+            throw new IllegalArgumentException("Hex color is incorrect");
+        }else{
+            this.hexValue=hexValue;
+        }
     }
 
-    public void setRouge(int rouge) throws IllegalArgumentException{
-        if(rouge>=0 || rouge<=255)
+    public void setRouge(int rouge){
+        if(isCorrectRGBValue(rouge)){
             this.rouge = rouge;
+        }else{
+            throw new IllegalArgumentException();
+        }
+
 
     }
-    public void setVert(int vert) throws IllegalArgumentException{
-        if(vert>=0 || vert<=255)
+    public void setVert(int vert){
+        if(isCorrectRGBValue(vert)){
             this.vert = vert;
+        }else{
+            throw new IllegalArgumentException();
+        }
     }
-    public void setBleu(int bleu)throws IllegalArgumentException {
-        if(bleu>=0 || bleu<=255)
+    public void setBleu(int bleu){
+        if(isCorrectRGBValue(bleu)){
             this.bleu = bleu;
+        }else{
+            throw new IllegalArgumentException();
+        }
 
     }
     public String toString(){
         return "[value:"+this.getHexValue() +", rouge:"+this.getRouge()+", vert:"+this.getVert()+", bleu:"+this.getBleu()+"]";
     }
 }
+
--- test/karima/color/model/ColorTest.java
+++ test/karima/color/model/ColorTest.java
@@ -17,6 +17,26 @@ class ColorTest {
     public void tearDown() {
         color = null;
     }
+
+    @Test
+    public void test_constructor_RGB() {
+        assertAll("Color object non conformes",
+                ()-> assertEquals(53, color.getRouge(),"getRed() est incorrect"),
+                ()-> assertEquals(213, color.getVert(),"getGreen() est incorrect"),
+                ()-> assertEquals(141, color.getBleu(), "getBlue() est incorrect"),
+                ()-> assertEquals("#35D58D", color.getHexValue(), "getHexValue() est incorrect"));
+    }
+
+    @Test
+    public void test_constructor_HexValue() {
+        color = new Color("#D58D35");
+        assertAll("Color object non conformes",
+                ()-> assertEquals(213, color.getRouge(),"getRed() est incorrect"),
+                ()-> assertEquals(141, color.getVert(),"getGreen() est incorrect"),
+                ()-> assertEquals(53, color.getBleu(), "getBlue() est incorrect"),
+                ()-> assertEquals("#D58D35", color.getHexValue(), "getHexValue() est incorrect"));
+    }
+
     @Test
     void testGetRouge() {
         assertEquals(53,color.getRouge());
@@ -71,7 +91,135 @@ class ColorTest {
     public void testToString() {
         assertEquals("[value:#35D58D, rouge:53, vert:213, bleu:141]", color.toString());
     }
+    @Test
+    public void test_rouge_value_smaller_than_0_Exception() {
+        assertThrows(IllegalArgumentException.class, () -> color = new Color(-3, 141, 53));
+    }
+
+    @Test
+    public void test_rouge_value_bigger_than_255_Exception() {
+        assertThrows(IllegalArgumentException.class, () -> color = new Color(264, 141, 53));
+    }
+
+    @Test
+    public void test_vert_value_smaller_than_0_Exception() {
+        assertThrows(IllegalArgumentException.class, () -> color = new Color(213, -31, 53));
+    }
+
+    @Test
+    public void test_vert_value_bigger_than_255_Exception() {
+        assertThrows(IllegalArgumentException.class, () -> color = new Color(213, 421, 53));
+    }
+
+    @Test
+    public void test_bleu_value_smaller_than_0_Exception() {
+        assertThrows(IllegalArgumentException.class, () -> color = new Color(213, 141, -132));
+    }
+
+    @Test
+    public void test_bleu_value_bigger_than_255_Exception() {
+        assertThrows(IllegalArgumentException.class, () ->  color = new Color(213, 141, 533));
+    }
 
 
+
+    @Test
+    public void test_HexValue_with_null_value_Exception() {
+        assertThrows(IllegalArgumentException.class, () -> color = new Color(null));
+    }
+
+    @Test
+    public void test_HexValue_with_wrong_value_1_Exception() {
+        assertThrows(IllegalArgumentException.class, () -> color = new Color("A26F76"));
+    }
+
+    @Test
+    public void test_HexValue_with_wrong_value_2_Exception() {
+        assertThrows(IllegalArgumentException.class, () -> color = new Color("#a26F76"));
+    }
+
+    @Test
+    public void test_HexValue_with_wrong_value_3_Exception() {
+        assertThrows(IllegalArgumentException.class, () -> color = new Color("#G26F76"));
+    }
+
+    @Test
+    public void test_HexValue_with_wrong_value_4_Exception() {
+        assertThrows(IllegalArgumentException.class, () -> color = new Color("#A26F7"));
+    }
+
+    @Test
+    public void test_HexValue_with_wrong_value_5_Exception() {
+        assertThrows(IllegalArgumentException.class, () -> color = new Color("#A26F71E"));
+    }
+
+    @Test
+    public void testToString_not_null() {
+        assertNotNull(color.toString(), "toString() est null");
+    }
+
+    @Test
+    public void testSetRouge_with_value_bigger_than_255_Exception() {
+        assertThrows(IllegalArgumentException.class, () -> color.setRouge(425));
+    }
+
+    @Test
+    public void testSetRouge_with_value_smaller_than_0_Exception() {
+        assertThrows(IllegalArgumentException.class, () ->color.setRouge(-12));
+    }
+
+    @Test
+    public void testSetVert_with_value_bigger_than_255_Exception() {
+        assertThrows(IllegalArgumentException.class, () ->color.setVert(615));
+    }
+
+    @Test
+    public void testSetVert_with_value_smaller_than_0_Exception() {
+        assertThrows(IllegalArgumentException.class, () ->color.setVert(-132));
+    }
+    @Test
+    public void testSetBleu_with_value_bigger_than_255_Exception() {
+        assertThrows(IllegalArgumentException.class, () ->color.setBleu(731));
+    }
+
+    @Test
+    public void testSetBleu_with_value_smaller_than_0_Exception() {
+        assertThrows(IllegalArgumentException.class, () ->color.setBleu(-1));
+    }
+
+    @Test
+    public void testSetHexValue_with_null_value_Exception() {
+        assertThrows(IllegalArgumentException.class, () -> color.setHexValue(null));
+    }
+
+    @Test
+    public void testSetHexValue_with_wrong_value_1_Exception() {
+        assertThrows(IllegalArgumentException.class, () ->color.setHexValue("A26F76"));
+    }
+
+    @Test
+    public void testSetHexValue_with_wrong_value_2_Exception() {
+        assertThrows(IllegalArgumentException.class, () ->color.setHexValue("#a26F76"));
+    }
+
+    @Test
+    public void testSetHexValue_with_wrong_value_3_Exception() {
+        assertThrows(IllegalArgumentException.class, () ->color.setHexValue("#G26F76"));
+    }
+
+    @Test
+    public void testSetHexValue_with_wrong_value_4_Exception() {
+        assertThrows(IllegalArgumentException.class, () ->color.setHexValue("#A26F7"));
+    }
+
+    @Test
+    public void testSetHexValue_with_wrong_value_5_Exception() {
+        assertThrows(IllegalArgumentException.class, () ->color.setHexValue("#A26F71E"));
+    }
 
 }
+
+
+
+
+
